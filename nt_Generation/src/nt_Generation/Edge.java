693066ed7cb65b8911c96b735b7fa3eed42ca207
Jana Jungbluth <s8jajung@stud.uni-saarland.de>
2018-07-20 12:28:58 +0200

    Erster Ansatz für die Erstellung des nt-Files in Java

---
+++ nt_Generation/src/nt_Generation/Edge.java
@@ -0,0 +1,8 @@
+package nt_Generation;
+
+public class Edge {
+
+	public Node start_node;
+	public Node end_node;
+
+}
---
+++ nt_Generation/src/nt_Generation/Variable.java
@@ -0,0 +1,10 @@
+package nt_Generation;
+
+public class Variable {
+	
+	public String name;
+	public String value;
+	public Supernode domain;
+	public Type range;
+
+}
