16d8d9bfa1200f130bd4519f6099901e6d48b385
Jana Jungbluth <s8jajung@stud.uni-saarland.de>
2018-07-31 13:33:42 +0200

    deleted nt generation

--- nt_Generation/src/nt_Generation/SceneMakerAutomaton.java
+++
@@ -1,9 +0,0 @@
-package nt_Generation;
-import java.util.Set;
-
-public class SceneMakerAutomaton extends Supernode {
-	
-	public Set<Variable> all_variables;
-	public Set<Supernode> all_supernodes;
-
-}
