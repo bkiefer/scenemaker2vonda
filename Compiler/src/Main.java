298d6ca41cb1f05a770d7cbda9ddd0af56016da7
Jana Jungbluth <s8jajung@stud.uni-saarland.de>
2018-07-31 10:33:20 +0200

    mistake in ontology uri fixed

--- Compiler/src/Main.java
+++ Compiler/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
     @SuppressWarnings("unused")
     SceneMakerAutomaton a = parser.getSceneMakerAutomaton();
     
-    ntFileGenerator ntg = new ntFileGenerator(a, "<http://www.semanticweb.org/ontologies/2018/6/untitled-ontology-6>", "./ontology/ontology.nt");
+    ntFileGenerator ntg = new ntFileGenerator(a, "http://www.semanticweb.org/jana/ontologies/2018/6/untitled-ontology-6", "./ontology/ontology.nt");
     
     ntg.generateNtFile();
     
