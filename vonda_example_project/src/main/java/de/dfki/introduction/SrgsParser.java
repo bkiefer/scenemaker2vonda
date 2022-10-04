d650b5da673be689ece4145948ce49b3f1e68e2f
Bernd Kiefer <j_a_w_bone@gmx.de>
2022-10-04 11:08:30 +0200

    Fix SrgsParser

--- vonda_example_project/src/main/java/de/dfki/introduction/SrgsParser.java
+++ vonda_example_project/src/main/java/de/dfki/introduction/SrgsParser.java
@@ -1,26 +1,28 @@
 package de.dfki.introduction;
 
-import static de.dfki.introduction.Constants.*;
+import static de.dfki.introduction.Constants.CFG_SRGS_GRAMMAR;
+import static de.dfki.introduction.Constants.DA_SLOT;
+import static de.dfki.introduction.Constants.PROP_SLOT;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
 import org.json.JSONObject;
-import org.jvoicexml.processor.srgs.ChartGrammarChecker;
-import org.jvoicexml.processor.srgs.ChartGrammarChecker.ChartNode;
-import org.jvoicexml.processor.srgs.JVoiceXmlGrammarManager;
-import org.jvoicexml.processor.srgs.grammar.Grammar;
-import org.jvoicexml.processor.srgs.grammar.GrammarException;
+import org.jvoicexml.processor.AbstractParser;
+import org.jvoicexml.processor.AbstractParser.ChartNode;
+import org.jvoicexml.processor.JVoiceXmlGrammarManager;
+import org.jvoicexml.processor.SemanticsInterpreter;
+import org.jvoicexml.processor.grammar.Grammar;
+import org.jvoicexml.processor.srgs.GrammarException;
 
 import de.dfki.mlt.rudimant.agent.DialogueAct;
 import de.dfki.mlt.rudimant.agent.nlg.Interpreter;
-import de.dfki.mlt.srgsparser.JSInterpreter;
 
 public class SrgsParser extends Interpreter {
   JVoiceXmlGrammarManager manager;
   Grammar ruleGrammar;
-  ChartGrammarChecker checker;
+  AbstractParser checker;
 
   @SuppressWarnings("rawtypes")
   @Override
@@ -30,7 +32,7 @@ public class SrgsParser extends Interpreter {
     try {
       manager = new JVoiceXmlGrammarManager();
       ruleGrammar = manager.loadGrammar(new File(configDir, grammarName).toURI());
-      checker = new ChartGrammarChecker(manager);
+      checker = AbstractParser.getParser(manager);
     } catch (IOException | GrammarException ex){
       logger.error("Could not read grammar file {} because of {}",
           new File(configDir, grammarName), ex.toString());
@@ -46,8 +48,7 @@ public class SrgsParser extends Interpreter {
       //TODO: Find out why no validRule is returned
       ChartNode validRule = checker.parse(ruleGrammar, tokens);
       if (validRule != null) {
-        JSInterpreter walker = new JSInterpreter(checker);
-        JSONObject object = walker.evaluate(validRule);
+        JSONObject object = SemanticsInterpreter.interpret(checker, validRule);
         String da = object.getString(DA_SLOT);
         if (da == null) return null;
         String prop = object.getString(PROP_SLOT);
