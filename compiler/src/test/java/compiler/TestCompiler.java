24cddd12d7070329363237098094e323375d5a80
Bernd Kiefer <j_a_w_bone@gmx.de>
2023-07-25 18:21:10 +0200

    Final fixes

--- compiler/src/test/java/compiler/TestCompiler.java
+++ compiler/src/test/java/compiler/TestCompiler.java
@@ -9,7 +9,7 @@ public class TestCompiler {
   @Test
   public void test() {
     String[] args = {
-        "../../scm2vonda/src/test/resources/introduction/sceneflow.xml",
+        "src/test/resources/introduction/sceneflow.xml",
         "../vonda_example_project", "true", "false"
     };
     Main.main(args);
